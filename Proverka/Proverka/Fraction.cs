4e53ee35a9d598cd285c3ceb860df0a62d5edd91
Nadi13 <113156746+Nadi13@users.noreply.github.com>
2022-11-28 17:28:10 +0600

    Update Fraction.cs

--- Proverka/Proverka/Fraction.cs
+++ Proverka/Proverka/Fraction.cs
@@ -8,8 +8,87 @@
         {
             this.numerator = numerator;
             this.denominator = denominator;
-
         }
+        public static Fraction Sum(Fraction a, Fraction b)
+        {
+            var c = new Fraction(1, 1);
+            if (a.denominator == b.denominator)
+            {
+                c.numerator = a.numerator + b.numerator;
+                c.denominator = a.denominator;
+            }
+            else
+            {
+                c.numerator = a.numerator * b.denominator + b.numerator * a.denominator;
+                c.denominator = a.denominator * b.denominator;
+            }
+            Fraction.Reduction(c);
+            return c;
+        }
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            return Sum(a, b);
+        }
+
+        public static Fraction Sub(Fraction a, Fraction b)
+        {
+            var c = new Fraction(1, 1);
+            if (a.denominator == b.denominator)
+            {
+                c.numerator = a.numerator - b.numerator;
+                c.denominator = a.denominator;
+            }
+            else
+            {
+                c.numerator = a.numerator * b.denominator - b.numerator * a.denominator;
+                c.denominator = a.denominator * b.denominator;
+            }
+            Fraction.Reduction(c);
+            return c;
+        }
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            return Sub(a, b);
+        }
+        public static Fraction Multi(int a, Fraction b)
+        {
+            var c = new Fraction(1, 1);
+            c.numerator = a * b.numerator;
+            c.denominator = b.denominator;
+            Fraction.Reduction(c);
+            return c;
+        }
+        public static Fraction operator *(int a, Fraction b)
+        {
+            return Multi(a, b);
+        }
+
+        public static Fraction Reduction(Fraction a)
+        {
+            int n = Math.Abs(a.numerator);
+            int d = Math.Abs(a.denominator);
+            while(n != d)
+            {
+                if(n > d)
+                {
+                    n = n - d;
+                }
+                else
+                {
+                    d = d - n;
+                }
+            }
+            int nod = d;
+            a.numerator = a.numerator / nod;
+            a.denominator = a.denominator / nod;
+            if ((a.denominator < 0))
+            {
+                a.numerator = -1 * (a.numerator);
+                a.denominator = Math.Abs(a.denominator);
+            }
+            return (a);
+        }
+
         public static bool operator ==(Fraction a, Fraction b)
         {
             return a.numerator == b.numerator && a.denominator == b.denominator;

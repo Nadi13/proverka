7f6a918a2205f3c0389c9d767c5f1a784d5ea3e2
Nadi13 <nadia_yudintseva@mail.ru>
2022-11-28 17:07:00 +0600

    commit

--- Proverka/Proverka/Fraction.cs
+++ Proverka/Proverka/Fraction.cs
@@ -10,86 +10,6 @@
             this.denominator = denominator;
 
         }
-
-        public static Fraction Sum(Fraction a, Fraction b)
-        {
-            var c = new Fraction(1, 1);
-            if (a.denominator == b.denominator)
-            {
-                c.numerator = a.numerator + b.numerator;
-                c.denominator = a.denominator;
-            }
-            else
-            {
-                c.numerator = a.numerator * b.denominator + b.numerator * a.denominator;
-                c.denominator = a.denominator * b.denominator;
-            }
-            Fraction.Reduction(c);
-            return c;
-        }
-        public static Fraction operator +(Fraction a, Fraction b)
-        {
-            return Sum(a, b);
-        }
-
-        public static Fraction Sub(Fraction a, Fraction b)
-        {
-            var c = new Fraction(1, 1);
-            if (a.denominator == b.denominator)
-            {
-                c.numerator = a.numerator - b.numerator;
-                c.denominator = a.denominator;
-            }
-            else
-            {
-                c.numerator = a.numerator * b.denominator - b.numerator * a.denominator;
-                c.denominator = a.denominator * b.denominator;
-            }
-            Fraction.Reduction(c);
-            return c;
-        }
-        public static Fraction operator -(Fraction a, Fraction b)
-        {
-            return Sub(a, b);
-        }
-        public static Fraction Multi(int a, Fraction b)
-        {
-            var c = new Fraction(1, 1);
-            c.numerator = a * b.numerator;
-            c.denominator = b.denominator;
-            Fraction.Reduction(c);
-            return c;
-        }
-        public static Fraction operator *(int a, Fraction b)
-        {
-            return Multi(a, b);
-        }
-
-        public static Fraction Reduction(Fraction a)
-        {
-            int n = Math.Abs(a.numerator);
-            int d = Math.Abs(a.denominator);
-            while (n != d)
-            {
-                if (n > d)
-                {
-                    n = n - d;
-                }
-                else
-                {
-                    d = d - n;
-                }
-            }
-            int nod = d;
-            a.numerator = a.numerator / nod;
-            a.denominator = a.denominator / nod;
-            if ((a.denominator < 0))
-            {
-                a.numerator = -1 * (a.numerator);
-                a.denominator = Math.Abs(a.denominator);
-            }
-            return (a);
-        }
         public static bool operator ==(Fraction a, Fraction b)
         {
             return a.numerator == b.numerator && a.denominator == b.denominator;
@@ -99,6 +19,17 @@
         {
             return (a == b) ? false : true;
         }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(numerator, denominator);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Fraction;
+        }
+
 
     }
 }
--- Tests/Fraction.cs
+++ Tests/Fraction.cs
@@ -6,48 +6,6 @@ namespace Tests
     {
 
         [Test]
-        public void TestSum()
-        {
-            var a = new Fraction(1, 2);
-            var b = new Fraction(1, 3);
-            var c = new Fraction(5, 6);
-            var d = new Fraction(2, 6);
-            Assert.True(c == a + b);
-            Assert.False(d == a + c);
-
-        }
-
-        [Test]
-        public void TestSub()
-        {
-            var a = new Fraction(-5, 10);
-            var b = new Fraction(1, 6);
-            var c = new Fraction(-8, 6);
-            var d = new Fraction(-2, 3);
-            Assert.True(d == a - b);
-            Assert.False(a == b - c);
-        }
-
-        [Test]
-        public void TestMulti()
-        {
-            var a = new Fraction(1, -2);
-            var b = new Fraction(-2, 1);
-
-            Assert.True(b == 4 * a);
-            Assert.False(b == -5 * a);
-        }
-
-        [Test]
-        public void TestReduction()
-        {
-            var a = new Fraction(2, -4);
-            var b = new Fraction(-1, 2);
-            var c = new Fraction(2, 4);
-            Assert.True(b == Fraction.Reduction(a));
-            Assert.False(c == Fraction.Reduction(a));
-        }
-        [Test]
         public void TestEquality()
         {
             var a = new Fraction(1, 2);
@@ -65,6 +23,21 @@ namespace Tests
             var c = new Fraction(1, 5);
             Assert.True(a != c);
             Assert.False(a != b);
+        }
+        [Test]
+        public void TestGetHashCode()
+        {
+            var a = new Fraction(1, 2);
+            Assert.IsInstanceOf<int>(a.GetHashCode());
+        }
+        [Test]
+        public void TestEquals()
+        {
+            var a = new Fraction(2, 3);
+            var b = new Fraction(1, 2);
+            var c = 3;
+            Assert.True(a.Equals(b));
+            Assert.False(a.Equals(c));
         }
     }
 }
